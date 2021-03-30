915decf154960514b9dc4d784236908521787a37
igoradamenko <mail@igoradamenko.com>
2021-03-30 22:12:28 +0300

    Fix build script: add popup/ folder

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,3 +1,10 @@
 VERSION=$(node -e 'console.log(require("./package.json").version)')
 
-zip -j $VERSION.zip CHANGELOG.md LICENSE README.md extension/*
+rm -rf tmp $VERSION.zip
+mkdir tmp
+
+cp -r CHANGELOG.md LICENSE README.md extension/* tmp
+
+cd tmp && zip ../$VERSION.zip ./* ./**/* && cd ..
+
+rm -rf tmp
